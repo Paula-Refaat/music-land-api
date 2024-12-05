e6af0d6b2e7a8bfebae8c81d4f736cf0d60e2a70
Paula-Refaat <poularefaat86@gmail.com>
2024-12-05 13:51:04 +0200

    🔖 root module with the following dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from './config';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { ArtistModule } from './modules/artist/artist.module';
+import { MusicianModule } from './modules/musician/musician.module';
+import { FavoriteModule } from './modules/favorite/favorite.module';
+import { PlaylistModule } from './modules/playlist/playlist.module';
+import { MusicModule } from './modules/music/music.module';
+import { AlbumModule } from './modules/album/album.module';
+import { MusicianAlbumModule } from './modules/musician-album/musician-album.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(config.db as TypeOrmModuleOptions),
+    AuthModule,
+    ProfileModule,
+    ArtistModule,
+    MusicianModule,
+    FavoriteModule,
+    PlaylistModule,
+    MusicModule,
+    AlbumModule,
+    MusicianAlbumModule,
+    NotificationsModule,
+  ],
   controllers: [],
   providers: [],
 })
